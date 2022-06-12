a13ca522247c6b2460bd4e1c182bda772064af8f
LEESOLL <57247409+LEESOLL@users.noreply.github.com>
2022-06-12 23:18:36 +0900

    6/12_study

---
+++ Inflearn_JAVACT/src/Section06/Main3.java
@@ -0,0 +1,27 @@
+package Section06;
+import java.util.*;
+
+public class Main3 {//삽입 정렬
+	static int[] solution(int n, int[] arr) {
+		for(int i=1; i<n; i++) {
+			int tmp = arr[i], j;
+			for(j=i-1; j>=0; j--) {
+				if(arr[j]>tmp)
+					arr[j+1]=arr[j];
+				else
+					break;
+			}
+			arr[j+1] = tmp;
+		}
+		return arr;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++)
+			arr[i] = sc.nextInt();
+		for(int x : solution(n,arr))
+			System.out.print(x+" ");
+	}
+}
